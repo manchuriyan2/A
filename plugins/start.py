c7f4a77fdb1144d829a09eb731d921db3ca9af1b
nero-bc <126649764+nero-bc@users.noreply.github.com>
2024-07-10 21:40:42 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -181,10 +181,10 @@ async def start_command(client: Client, message: Message):
 async def not_joined(client: Client, message: Message):
     buttons = [
         [
-            InlineKeyboardButton("Join 1", url=f"https://t.me/+LDdjfyT53hdiYzE1"),
+            InlineKeyboardButton("Join 1", url=f"https://t.me/+KvZCcSyOZ2plZTc9"),
             InlineKeyboardButton("Join 2", url=client.invitelink),
-            InlineKeyboardButton("Join 3", url=client.invitelink2),
-            InlineKeyboardButton("Join 4", url=f"https://t.me/+R6xc_7a0yX4xYzVl")
+            InlineKeyboardButton("Join 3", url=f"https://t.me/+rXrYdP3-KzRlYTI9"),
+            InlineKeyboardButton("Join 4", url=f"https://t.me/+e2XqhuSiab9iM2E1")
         ]
     ]
     try:
