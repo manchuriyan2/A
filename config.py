7dbc19c94d27f4250e9fb708d409300502451253
nero-bc <126649764+nero-bc@users.noreply.github.com>
2024-08-14 13:40:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,31 +11,31 @@ from logging.handlers import RotatingFileHandler
 
 
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002020304266")) #hanime
-FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1001824105670")) #anime download
+FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1002020304266")) #anime download
 
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7227898838:AAH0GGaA1UdZVIpJLlYm5g6yj8pvfuSCmVo") #@Anime_Elixir1_Bot
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6737569405:AAH7tH0Hrax_8M-8SiN2s4UzD--SLS1Mdxc") #@Hentai
 APP_ID = int(os.environ.get("APP_ID", "25695562"))
 API_HASH = os.environ.get("API_HASH", "0b691c3e86603a7e34aae0b5927d725a")
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001902545745"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001929558021"))
 OWNER_ID = int(os.environ.get("OWNER_ID", "1895952308"))
 
 PORT = os.environ.get("PORT", "8080")
 DB_URL = os.environ.get("DATABASE_URL", "mongodb+srv://skiliggeeXporter:skiliggeeXporter@cluster0.tdxtakc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
+DB_NAME = os.environ.get("DATABASE_NAME", "AdultElixir")
 
 
 TIME = int(os.environ.get("TIME", "3600"))
 USE_SHORTLINK = True if os.environ.get('USE_SHORTLINK', "TRUE") == "TRUE" else False 
-SHORTLINK_API_URL = os.environ.get("SHORTLINK_API_URL", "modijiurl.com")
-SHORTLINK_API_KEY = os.environ.get("SHORTLINK_API_KEY", "1f37e24233e5b28fc9dba3b11f6a2be6277ca186")
-VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', "43200")) # Add time in seconds
-TUT_VID = os.environ.get("TUT_VID","https://t.me/Anime_Elixir/12")
-USE_PAYMENT = True if (os.environ.get("USE_PAYMENT", "TRUE") == "TRUE") & (USE_SHORTLINK) else False
+SHORTLINK_API_URL = os.environ.get("SHORTLINK_API_URL", "runurl.in")
+SHORTLINK_API_KEY = os.environ.get("SHORTLINK_API_KEY", "3a2c084d61d0813b05a00cd9ba564e92e39f92cb")
+VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', "86400")) # Add time in seconds
+TUT_VID = os.environ.get("TUT_VID","https://t.me/Anime_Elixir/8")
+USE_PAYMENT = True if (os.environ.get("USE_PAYMENT", "FALSE") == "TRUE") & (USE_SHORTLINK) else False
 
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "8"))
 
-START_MSG = os.environ.get("START_MESSAGE", "<blockquote><b>ℹ️ Hello {mention}\nI can store private files in Specified Channel and other users can access it from special link. 💾</b></blockquote>")
+START_MSG = os.environ.get("START_MESSAGE", "<blockquote><b>ℹ️ Hello {mention} Welcome to our 18+ Contact Provider Bot. Exclusively work for <a href='https://t.me/Adult_Elixir'>Elixir of Lust</a></b></blockquote>")
 FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "<blockquote><b>ℹ️ Hello {mention}\nYou need to join in my Channel to use me\nKindly Please join Channel</b></blockquote>")
 try:
     ADMINS=[]
